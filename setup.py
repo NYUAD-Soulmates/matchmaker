ac849db3473878203de17349a873dcdb87440a97
Benjamin Pang <benjinpang@gmail.com>
2021-02-23 09:21:20 +0400

    quickstart

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nyuad-matchmaker',
+    packages=find_packages(include=['nyuad-matchmaker']),
+    version='0.0.0',
+    description='Matchmaking algorithm for NYUAD',
+    author='NYUAD Soulmates Team',
+    license='MIT',
+    install_requires=['matching'],
+    setup_requires=[],
+    tests_require=[],
+    test_suite='tests',
+)
