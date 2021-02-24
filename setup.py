5a9158b03227647ccf74186e9a8102f64ae222da
Benjamin Pang <benjinpang@gmail.com>
2021-02-24 10:14:27 +0400

    add numpy

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Matchmaking algorithm for NYUAD students',
     author='NYUAD Soulmates Team',
     license='MIT',
-    install_requires=['matching'],
+    install_requires=['numpy', 'matching'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
